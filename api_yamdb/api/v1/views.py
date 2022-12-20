51512a905b3ed7518ca3bf21f2910a4ebf0792ff
Mikhail Kostrov <109241607+mdkostrov@users.noreply.github.com>
2022-12-20 22:56:29 +0300

    Update views.py
    'кавычки' вместо "таких"

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -169,7 +169,7 @@ class ReviewViewSet(ModelViewSet):
     permission_classes = [IsAuthor | IsModerator | IsAdminUpdate]
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.reviews.all()
 
     def perform_create(self, serializer):
